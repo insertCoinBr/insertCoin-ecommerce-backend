d00b0d73268b896bb6ce192b7ea1278e58fb4a90
AndersonBohnem <bohnembergeranderson@gmail.com>
2025-11-08 22:11:53 -0300

    feat: add swagger by project

--- insertcoin-auth-service/src/main/java/org/insertcoin/insertcoin_auth_service/configs/ConfigSecurity.java
+++ insertcoin-auth-service/src/main/java/org/insertcoin/insertcoin_auth_service/configs/ConfigSecurity.java
@@ -2,7 +2,6 @@ package org.insertcoin.insertcoin_auth_service.configs;
 
 import org.insertcoin.insertcoin_auth_service.components.AuthTokenFilter;
 import org.insertcoin.insertcoin_auth_service.services.CustomUserDetailsService;
-import org.insertcoin.insertcoin_auth_service.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +11,12 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +26,6 @@ public class ConfigSecurity {
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
-
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
@@ -37,30 +34,6 @@ public class ConfigSecurity {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/auth/signin",
-                                "/auth/signup",
-                                "/auth/verify-email",
-                                "/auth/validate-code",
-                                "/auth/reset-password",
-                                "/auth/forgot-password",
-                                "/auth/me/update"
-                        ).permitAll()
-                        .requestMatchers("/error").permitAll()
-                        .requestMatchers("/auth/admin/**").authenticated()
-                        .anyRequest().authenticated()
-                )
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
     }
 
     @Bean
@@ -74,5 +47,38 @@ public class ConfigSecurity {
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        // 🔓 Endpoints públicos
+                        .requestMatchers(
+                                "/auth/signin",
+                                "/auth/signup",
+                                "/auth/verify-email",
+                                "/auth/validate-code",
+                                "/auth/reset-password",
+                                "/auth/forgot-password",
+                                "/auth/me/update",
+                                // Swagger + OpenAPI
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/api-docs/**",
+                                "/docs/**",
+                                "/error"
+                        ).permitAll()
+                        // 🔒 Endpoints protegidos
+                        .requestMatchers("/auth/admin/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
